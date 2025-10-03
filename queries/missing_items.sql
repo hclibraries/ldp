b2bd84be2e3d030f5a72d0f8c81ea1ef7453fe2a
hclibraries <113636312+hclibraries@users.noreply.github.com>
2025-10-03 17:43:15 -0400

    Update missing_items.sql

--- queries/missing_items.sql
+++ queries/missing_items.sql
@@ -1,7 +1,7 @@
 --metadb:function missing_items
 DROP FUNCTION IF EXISTS missing_items;
-CREATE FUNCTION missing_items(start_date DATE DEFAULT '2000-01-01', 
-                              end_date DATE DEFAULT '2050-01-01')
+CREATE FUNCTION missing_items(start_date DATE DEFAULT '2000', 
+                              end_date DATE DEFAULT '2050')
 RETURNS TABLE(
     title TEXT,
     barcode TEXT,
